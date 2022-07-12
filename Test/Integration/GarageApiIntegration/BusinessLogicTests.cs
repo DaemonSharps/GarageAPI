40e047e4d4ce73ad38e1770d56b934e0b4814a9c
DaemonSharps <smirnov.god@gmail.com>
2022-07-13 01:20:58 +0300

    окружение для интеграционных тестов

---
+++ Test/Integration/GarageApiIntegration/BusinessLogicTests.cs
@@ -0,0 +1,15 @@
+﻿using GarageAPI;
+using GarageApiIntegration.Common;
+
+namespace GarageApiIntegration;
+
+public class BusinessLogicTests : ApiTestBase
+{
+    public BusinessLogicTests(GarageApiTestFixture<Startup> fixture) : base(fixture) { }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
