c247eb9e6281acb27e592568977decce0914da8c
DaemonSharps <smirnov.god@gmail.com>
2022-07-12 22:25:14 +0300

    Для дебага использовать InMemoryDB

--- Startup.cs
+++ Startup.cs
@@ -16,74 +16,82 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GarageAPI
+namespace GarageAPI;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+#if DEBUG
+        services.AddDbContext<GarageDBContext>(builder => builder.UseInMemoryDatabase("Garage_DB_Test"));
+#else
+        services.AddDbContext<GarageDBContext>(builder =>
+                builder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")),
+                b => b.MigrationsAssembly(typeof(Startup).Assembly.GetName().Name)));
+#endif
+
+        services.AddTransient<IRecordsService, RecordsService>();
+        services.AddTransient<ICustomerService, CustomerService>();
+
+        services.AddCors();
+
+        services.AddSwaggerGen(setup =>
         {
-            Configuration = configuration;
-        }
+            setup.SwaggerDoc(
+                "v1.0",
+                new OpenApiInfo
+                {
+                    Title = "Garage API",
+                    Version = "1.0"
+                });
+            setup.CustomSchemaIds(type => type.ToString());
+            var filePath = Path.Combine(AppContext.BaseDirectory, "GarageApiDocumentation.xml");
+            setup.IncludeXmlComments(filePath);
+        });
+    }
 
-        public IConfiguration Configuration { get; }
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            services.AddControllers();
-
-            services.AddDbContext<GarageDBContext>(builder =>
-                    builder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")), ServiceLifetime.Transient);
-
-            services.AddTransient<IRecordsService, RecordsService>();
-            services.AddTransient<ICustomerService, CustomerService>();
-
-            services.AddCors();
-
-            services.AddSwaggerGen(setup =>
-            {
-                setup.SwaggerDoc(
-                    "v1.0",
-                    new OpenApiInfo
-                    {
-                        Title = "Garage API",
-                        Version = "1.0"
-                    });
-                setup.CustomSchemaIds(type => type.ToString());
-                var filePath = Path.Combine(AppContext.BaseDirectory, "GarageApiDocumentation.xml");
-                setup.IncludeXmlComments(filePath);
-            });
+            app.UseDeveloperExceptionPage();
         }
+#if DEBUG
+        using (var scope = app.ApplicationServices.CreateScope())
+        using (var context = scope.ServiceProvider.GetRequiredService<GarageDBContext>())
+            context.Database.EnsureCreated();
+#endif
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        app.UseSwagger();
+        app.UseRouting();
+        app.UseSwaggerUI(opt =>
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            opt.SwaggerEndpoint("/swagger/v1.0/swagger.json", "Garage API");
+            opt.RoutePrefix = string.Empty;
+        });
 
-            app.UseSwagger();
-            app.UseRouting();
-            app.UseSwaggerUI(opt =>
-            {
-                opt.SwaggerEndpoint("/swagger/v1.0/swagger.json", "Garage API");
-                opt.RoutePrefix = string.Empty;
-            });
-
-            app.UseAuthorization();
-            app.UseCors(options =>
-            {
-                options
-                .WithOrigins("https://vgarage.vercel.app", "http://localhost:3000")
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-            });
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-            });
-        }
+        app.UseAuthorization();
+        app.UseCors(options =>
+        {
+            options
+            .WithOrigins("https://vgarage.vercel.app", "http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapDefaultControllerRoute();
+        });
     }
 }
