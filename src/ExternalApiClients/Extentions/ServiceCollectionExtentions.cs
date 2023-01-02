bf6c25409e01e71f6d5ec7eb73ea703d02bf0252
DaemonSharps <smirnov.god@gmail.com>
2023-01-03 01:15:47 +0300

    Добавил клиент JWT провайдера

---
+++ src/ExternalApiClients/Extentions/ServiceCollectionExtentions.cs
@@ -0,0 +1,25 @@
+﻿using ExternalApiClients.Rest;
+using Microsoft.Extensions.DependencyInjection;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalApiClients.Extentions;
+
+public static class ServiceCollectionExtentions
+{
+    public static IServiceCollection AddRestService<TRestService>(this IServiceCollection services, Uri baseAddress)
+        where TRestService : class
+    {
+        services
+            .AddRefitClient<TRestService>()
+            .ConfigureHttpClient(c => c.BaseAddress = baseAddress);
+        return services;
+    }
+
+    public static IServiceCollection AddJwtProviderClient(this IServiceCollection services)
+        => services.AddRestService<IJwtProviderApi>(new Uri(Environment.GetEnvironmentVariable("JWT_PROVIDER_URI")));
+}
---
+++ src/ExternalApiClients/Rest/IJwtProviderApi.cs
@@ -0,0 +1,34 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalApiClients.Rest;
+
+public interface IJwtProviderApi
+{
+    [Post("/users")]
+    public TokenResponse RegisterUser([Body] RegisterUserRequest request);
+}
+
+public class RegisterUserRequest
+{
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Patronymic { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+}
+
+public class TokenResponse
+{
+    public string AccessToken { get; set; }
+
+    public Guid RefreshToken { get; set; }
+}
