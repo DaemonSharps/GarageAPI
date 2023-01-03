1c371c647c45ea651adb84538ca42c920600c3de
DaemonSharps <smirnov.god@gmail.com>
2023-01-04 00:04:19 +0300

    тест на авторизацию

---
+++ Test/Integration/GarageApiIntegration/AuthTests.cs
@@ -0,0 +1,61 @@
+﻿using ExternalApiClients.Rest;
+using GarageAPI;
+using GarageAPI.Controllers.Schemas;
+using GarageApiIntegration.Common;
+using GarageApiIntegration.Extentions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageApiIntegration;
+
+public class AuthTests : ApiTestBase
+{
+    public AuthTests(GarageApiTestFixture<Startup> fixture) : base(fixture) { }
+
+    private IJwtProviderApi CreateJwtMock()
+    {
+        var jwtProviderMock = new Mock<IJwtProviderApi>();
+        jwtProviderMock.Setup(j => j.RegisterUser(It.IsAny<RegisterUserRequest>(), default))
+            .ReturnsAsync(new Refit.ApiResponse<TokenResponse>(
+                new HttpResponseMessage(System.Net.HttpStatusCode.OK),
+                new TokenResponse
+                {
+                    AccessToken = "123124124",
+                    RefreshToken = new Guid("4e264e55-b16d-416b-82bb-2cbbf2aee27b")
+                },
+                null,
+                null));
+        return jwtProviderMock.Object;
+    }
+
+    [Fact]
+    public async Task RegisterNewUser()
+    {
+        var registrationRequest = new RegisterUserRequest
+        {
+            Email = "1@mail.ru",
+            FirstName = "fn",
+            LastName = "ln",
+            Patronymic = "sn",
+            Password = "1"
+        };
+
+        var tokenModel = await Client.RegisterUser(registrationRequest);
+
+        Assert.NotEmpty(tokenModel.AccessToken);
+
+        var filterRequest = new GetUsersByFilterRequest
+        {
+            Email = registrationRequest.Email
+        };
+        var users = await Client.GetUsersByFilter(filterRequest);
+
+        Assert.Single(users);
+    }
+}
---
+++ Test/Integration/GarageApiIntegration/Extentions/HttpClientExtentions.Auth.cs
@@ -0,0 +1,24 @@
+﻿using ExternalApiClients.Rest;
+using GarageAPI.Controllers.Schemas;
+using System.Globalization;
+using System.Net.Http.Json;
+using DTO = GarageDataBase.DTO;
+
+namespace GarageApiIntegration.Extentions;
+
+public static partial class HttpClientExtentions
+{
+    public static async Task<TokenResponse> RegisterUser(this HttpClient client, RegisterUserRequest request)
+    {
+        var response = await client.PostAsJsonAsync("/api/auth", request);
+        response.EnsureSuccessStatusCode();
+        var accessToken = await response.Content.ReadFromJsonAsync<string>();
+        var refreshToken = response.Content.Headers.FirstOrDefault(h => h.Key == "Set-Cookie").Value;
+
+        return new TokenResponse
+        { 
+            AccessToken = accessToken 
+        };
+    }
+}
+
