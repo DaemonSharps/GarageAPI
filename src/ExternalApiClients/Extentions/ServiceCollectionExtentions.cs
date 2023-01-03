9a110a79ceed54e215de373976a9e34fc52ce14e
DaemonSharps <smirnov.god@gmail.com>
2023-01-03 22:29:05 +0300

    поправил регистрацию сервисов через refit

--- src/ExternalApiClients/Extentions/ServiceCollectionExtentions.cs
+++ src/ExternalApiClients/Extentions/ServiceCollectionExtentions.cs
@@ -11,15 +11,21 @@ namespace ExternalApiClients.Extentions;
 
 public static class ServiceCollectionExtentions
 {
-    public static IServiceCollection AddRestService<TRestService>(this IServiceCollection services, Uri baseAddress)
+    public static IServiceCollection AddRestService<TRestService>(this IServiceCollection services, string baseAddress)
         where TRestService : class
     {
         services
             .AddRefitClient<TRestService>()
-            .ConfigureHttpClient(c => c.BaseAddress = baseAddress);
+            .ConfigureHttpClient(c => 
+            {
+                if (!string.IsNullOrEmpty(baseAddress))
+                {
+                    c.BaseAddress = new Uri(baseAddress);
+                }
+            });
         return services;
     }
 
     public static IServiceCollection AddJwtProviderClient(this IServiceCollection services)
-        => services.AddRestService<IJwtProviderApi>(new Uri(Environment.GetEnvironmentVariable("JWT_PROVIDER_URI")));
+        => services.AddRestService<IJwtProviderApi>(Environment.GetEnvironmentVariable("JWT_PROVIDER_URI"));
 }
