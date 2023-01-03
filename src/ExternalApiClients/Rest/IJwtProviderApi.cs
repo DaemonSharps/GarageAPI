67ea215dce7a1979d347b966b2cbf1b43c99016a
DaemonSharps <smirnov.god@gmail.com>
2023-01-03 21:25:13 +0300

    Добавил ограничений на модель

--- src/ExternalApiClients/Rest/IJwtProviderApi.cs
+++ src/ExternalApiClients/Rest/IJwtProviderApi.cs
@@ -1,6 +1,7 @@
 ﻿using Refit;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace ExternalApiClients.Rest;
 public interface IJwtProviderApi
 {
     [Post("/users")]
-    public Task<ApiResponse<TokenResponse>> RegisterUser([Body] RegisterUserRequest request);
+    public Task<ApiResponse<TokenResponse>> RegisterUser([Body] RegisterUserRequest request, CancellationToken cancellationToken);
 }
 
 public class RegisterUserRequest
@@ -21,8 +22,10 @@ public class RegisterUserRequest
 
     public string Patronymic { get; set; }
 
+    [EmailAddress]
     public string Email { get; set; }
 
+    [Required]
     public string Password { get; set; }
 }
 
