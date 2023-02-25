83ab5a6dfec24689df8e4461535344b4ff76cdbc
DaemonSharps <smirnov.god@gmail.com>
2023-02-26 00:31:56 +0300

    Добавил авторизацию через accessToken

---
+++ src/GarageApi/Options/TokenOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace GarageAPI.Options;
+
+public class TokenOptions
+{
+    /// <summary>
+    /// Секция в appsettings.json
+    /// </summary>
+    public const string Section = "Token";
+
+    /// <summary>
+    /// Ключ для генерации токена доступа
+    /// </summary>
+    public string AccessKey { get; set; }
+
+    /// <summary>
+    /// Униальный ключ издателя токена
+    /// </summary>
+    public string Issuer { get; set; }
+}
+
