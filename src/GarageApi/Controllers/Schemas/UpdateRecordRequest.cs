b2aae42ba6a7aac6c45eef43ee754a62a47d460b
Denis Smirnov <56834210+DaemonSharps@users.noreply.github.com>
2023-02-26 18:07:59 +0300

    Метод обновления записи (#34)

---
+++ src/GarageApi/Controllers/Schemas/UpdateRecordRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GarageAPI.Controllers.Schemas;
+
+public class UpdateRecordRequest
+{
+    public long RecordId { get; set; }
+
+    /// <summary>
+    /// Время записи
+    /// </summary>
+    [Required]
+    public string Time { get; set; }
+
+    /// <summary>
+    /// Место посадки
+    /// </summary>
+    [Required]
+    public int PlaceNumber { get; set; }
+}
+
